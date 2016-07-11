70ea611daf8c211bf987dd61932b9276f3a39fb2
swiftwill <prc-lh138-18@PRC-LH138-18s-iMac.local>
2016-07-11 16:05:58 -0500

    SwiftSuperCalc_v2
    added some comments; no change to code

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+//: simulated calculator
+//: a program by William Hill ;D
+
+import UIKit
+
+//  declare and initialize input variables
+var a = 10
+var b = 5
+var c = 7
+// variables for mathematical operations
+var addresults = 0
+var subresults = 0
+var multresults = 0
+var divresults = 0
+var modresults = 0
+
+
+// operator definitions for mathematical operations
+addresults = a + b
+subresults = a - b
+multresults = a * b
+divresults = a / b
+modresults = a % c
+
+
+// print results of mathematical operations to screen
+print("a + b = ", addresults, terminator:"")
+print("a - b = ", subresults, terminator:"")
+print("a * b = ", multresults, terminator:"")
+print("a / b = ", divresults, terminator:"")
+print("a % c = ", modresults, terminator:"")
+
+
